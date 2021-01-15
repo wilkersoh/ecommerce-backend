b2fbd7fcebd75ad9ae0596d1d858e759006364c2
wilkersoh <plgu@hotmail.com>
2021-01-15 22:17:57 +0800

    added relation

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,13 +1,8 @@
 "use strict";
-
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-  async findByCategory(ctx) {
-    console.log("ctx findByCategory", ctx);
-    return { product: 1, name: "hi" };
-  },
-};
+module.exports = {};
