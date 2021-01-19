2d8220b4527a9b9245cb6ad92049119d4e993291
wilkersoh <plgu@hotmail.com>
2021-01-19 15:06:56 +0800

    Update Authenticate

--- api/custom/controllers/Custom.js
+++ api/custom/controllers/Custom.js
@@ -3,10 +3,11 @@
 module.exports = {
   async logout(ctx) {
     ctx.cookies.set("token", null);
-    console.log("yz: user logout.");
+    console.log("yz: logout 1");
     ctx.send({
       authorized: true,
       message: "Successfully destroyed session",
     });
+    console.log("yz: logout 2");
   },
 };
