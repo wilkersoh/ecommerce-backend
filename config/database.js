82b77eebe8b728926490f168927008731a5ad2d3
wilkersoh <plgu@hotmail.com>
2021-01-23 21:31:46 +0800

    Magic config settings

--- config/database.js
+++ config/database.js
@@ -1,28 +1,28 @@
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
-    // default: {
-    //   connector: "bookshelf",
-    //   settings: {
-    //     client: "sqlite",
-    //     filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-    //   },
-    //   options: {
-    //     useNullAsDefault: true,
-    //   },
-    // },
     default: {
       connector: "bookshelf",
       settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "boilerplateEco"),
-        username: env("DATABASE_USERNAME", "root"),
-        password: env("DATABASE_PASSWORD", "password"),
-        ssl: env.bool("DATABASE_SSL", false),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
+    // default: {
+    //   connector: "bookshelf",
+    //   settings: {
+    //     client: "mysql",
+    //     host: env("DATABASE_HOST", "127.0.0.1"),
+    //     port: env.int("DATABASE_PORT", 3306),
+    //     database: env("DATABASE_NAME", "boilerplateEco"),
+    //     username: env("DATABASE_USERNAME", "root"),
+    //     password: env("DATABASE_PASSWORD", "password"),
+    //     ssl: env.bool("DATABASE_SSL", false),
+    //   },
+    //   options: {},
+    // },
   },
 });
