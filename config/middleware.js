a91c0913a99e82171eea988dda75c7ce8bc34ffb
wilkersoh <plgu@hotmail.com>
2021-01-25 17:21:12 +0800

    Updae middleware

--- config/middleware.js
+++ config/middleware.js
@@ -5,6 +5,7 @@ module.exports = {
       origin: [
         "http://localhost:3000",
         "http://localhost:1337",
+        "http://localhost:3003",
         "https://ecommerce-backend.staging.selfpaths.com",
         "https://ecommerce-frontend.staging.selfpaths.com",
       ],
