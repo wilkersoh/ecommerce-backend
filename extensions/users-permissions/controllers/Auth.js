a40ffeb6f249cbf1d6dfb4ec126f29db6d329334
wilkersoh <plgu@hotmail.com>
2021-01-25 15:27:31 +0800

    update cookeis attributes

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -547,13 +547,15 @@ module.exports = {
 
       ctx.cookies.set("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
+        // secure: process.env.NODE_ENV === "production" ? true : false,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
         sameSite: "lax",
-        domain:
-          process.env.NODE_ENV === "development"
-            ? "localhost"
-            : process.env.PRODUCTION_URL,
+        domain: "https://ecommerce-frontend.staging.selfpaths.com",
+        // domain:
+        //   process.env.NODE_ENV === "development"
+        //     ? "localhost"
+        //     : process.env.PRODUCTION_URL,
       });
       console.log("token: ", token);
       return ctx.send({
