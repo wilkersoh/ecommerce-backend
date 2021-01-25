eb98af12bd6ebbcf53bbb83f31681e027b8129d0
wilkersoh <plgu@hotmail.com>
2021-01-25 18:58:53 +0800

    Update URL

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -550,8 +550,8 @@ module.exports = {
         // secure: process.env.NODE_ENV === "production" ? true : false,
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-        sameSite: "lax",
-        domain: "https://ecommerce-backend.staging.selfpaths.com",
+        sameSite: "none",
+        domain: "https://ecommerce-frontend.staging.selfpaths.com",
         // domain:
         //   process.env.NODE_ENV === "development"
         //     ? "localhost"
@@ -565,9 +565,9 @@ module.exports = {
     } catch (err) {
       const adminError = _.includes(err.message, "username")
         ? {
-          id: "Auth.form.error.username.taken",
-          message: "Username already taken",
-        }
+            id: "Auth.form.error.username.taken",
+            message: "Username already taken",
+          }
         : { id: "Auth.form.error.email.taken", message: "Email already taken" };
 
       ctx.badRequest(null, formatError(adminError));
