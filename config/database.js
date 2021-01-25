94631a279b02b97e3d4300a0e1dbd52cce04545f
wilkersoh <plgu@hotmail.com>
2021-01-25 09:10:06 +0800

    change to root user in database

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 3306),
         database: env("DATABASE_NAME", "boilerplateEco"),
-        username: env("DATABASE_USERNAME", "selfpaths"),
+        username: env("DATABASE_USERNAME", "root"), // not work in selfpaths user
         password: env("DATABASE_PASSWORD", "Selfpaths@password123"),
         // username: env("DATABASE_USERNAME", "root"),
         // password: env("DATABASE_PASSWORD", "password"),
