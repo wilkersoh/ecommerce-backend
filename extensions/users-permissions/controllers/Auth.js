7e7e8797af82a0d6dc0dad55f6b183b0dd46151a
wilkersoh <plgu@hotmail.com>
2021-01-25 19:16:51 +0800

    Update login function cookies

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -140,7 +140,7 @@ module.exports = {
           // secure: process.env.NODE_ENV === "production" ? true : false,
           secure: true,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          domain: "https://ecommerce-frontend.staging.selfpaths.com/",
+          domain: "https://ecommerce-frontend.staging.selfpaths.com",
           // domain:
           //   process.env.NODE_ENV === "development"
           //     ? "localhost"
@@ -553,7 +553,7 @@ module.exports = {
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
         sameSite: "lax",
-        domain: "https://ecommerce-frontend.staging.selfpaths.com/",
+        domain: "https://ecommerce-frontend.staging.selfpaths.com",
         // domain:
         //   process.env.NODE_ENV === "development"
         //     ? "localhost"
