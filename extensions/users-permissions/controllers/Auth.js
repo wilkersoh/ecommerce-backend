ed11e2386f6fedfb31d286bc7bb26d279db9b37f
wilkersoh <plgu@hotmail.com>
2021-01-25 19:04:24 +0800

    SameSite chages to none

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -550,7 +550,7 @@ module.exports = {
         // secure: process.env.NODE_ENV === "production" ? true : false,
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-        sameSite: "lax",
+        sameSite: "none",
         domain: "https://ecommerce-frontend.staging.selfpaths.com/",
         // domain:
         //   process.env.NODE_ENV === "development"
