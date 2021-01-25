aaebf5174a6dc48bcd7c9ceca5577fe36f52381e
wilkersoh <plgu@hotmail.com>
2021-01-25 22:30:54 +0800

    change middleware cors

--- config/middleware.js
+++ config/middleware.js
@@ -5,8 +5,8 @@ module.exports = {
       origin: [
         "http://localhost:3000",
         "http://localhost:1337",
-        "https://ecommerce-backend.staging.selfpaths.com",
-        "https://ecommerce-frontend.staging.selfpaths.com",
+        ".ecommerce-backend.staging.selfpaths.com",
+        ".ecommerce-frontend.staging.selfpaths.com",
       ],
     },
   },
