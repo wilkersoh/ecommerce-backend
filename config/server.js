77e22e81c694270b66f8a084460bfd30d50cd86f
wilkersoh <plgu@hotmail.com>
2021-01-26 14:45:19 +0800

    remove sever.js proxy

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://ecommerce-frontend.staging.selfpaths.com",
-  proxy: true,
+  // url: "https://ecommerce-frontend.staging.selfpaths.com",
+  // proxy: true,
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ef3257ff3e2aa85e54e237f82f8a8c98"),
