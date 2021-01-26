e7aeb363f8ef57de32935e6581ff2b2108ca893d
wilkersoh <plgu@hotmail.com>
2021-01-26 14:47:21 +0800

    on proxy ture

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   // url: "https://ecommerce-frontend.staging.selfpaths.com",
-  // proxy: true,
+  proxy: true,
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ef3257ff3e2aa85e54e237f82f8a8c98"),
