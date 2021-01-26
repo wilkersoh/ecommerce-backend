458f619ddc4f724108ecf06734782fd5669e7150
wilkersoh <plgu@hotmail.com>
2021-01-26 21:57:01 +0800

    testing logout function

--- api/custom/controllers/Custom.js
+++ api/custom/controllers/Custom.js
@@ -3,7 +3,7 @@
 module.exports = {
   async logout(ctx) {
     ctx.cookies.set("token", null);
-
+    console.log("user trigger logout ");
     ctx.send({
       authorized: true,
       message: "Successfully Log out",
