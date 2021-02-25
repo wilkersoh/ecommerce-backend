c58787806df401561d48c757464641861c1b1fbd
wilkersoh <plgu@hotmail.com>
2021-02-25 23:39:58 +0800

    Update confimation email url

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   // url: "https://ecommerce-frontend.staging.selfpaths.com",
+  url: "http://localhost:1337",
   proxy: true,
   admin: {
     auth: {
